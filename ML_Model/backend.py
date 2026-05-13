ccf37e1007f416f3eb14d6459c8405f717b73214
Mahesh <mmaheshwagatkar@gmail.com>
2026-05-13 23:10:10 +0530

    Final7.0

--- ML_Model/backend.py
+++ ML_Model/backend.py
@@ -74,5 +74,8 @@ def predict():
         }), 500
 
 
-if __name__ == '__main__':
-    app.run(port=5002, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
